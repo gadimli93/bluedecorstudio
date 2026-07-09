ed62a9f5f67fe7378033af543065de67593c8b2f
Nushaba M. Gadimli <nushaba.gadimli@gmail.com>
2026-07-09 11:32:20 +0000

    fix: add CNAME file and update base path for custom domain

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
 
-const repoBase = "/bluedecorstudio/";
-
 export default defineConfig(({ mode }) => ({
-  base: mode === "production" ? repoBase : "/",
+  base: "/",
   plugins: [react(), tsconfigPaths()],
 }));
